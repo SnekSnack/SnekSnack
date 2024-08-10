e2acc945024af2c7559d378ddbd53c0dc99e0d7f
Hashcher <69722701+Hashcher@users.noreply.github.com>
2024-08-11 05:50:21 +1000

    draft url list

--- Backend/SnekSnackDev/SnekText/urls.py
+++ Backend/SnekSnackDev/SnekText/urls.py
@@ -3,6 +3,11 @@ from .views import *
  
  
 urlpatterns = [
-    path("", textList, name="textList"),
-
+    path("persona/list", chatlist, name="textList"),
+    path("persona/create", chatlist, name="textList"),
+    path("persona/view/<int:id>", chatlist, name="textList"),
+    # chat can be created when viewing the persona 
+    path("chat/<int:ChatRoomId>", chatlist, name="textList"),
+    path("login", chatlist, name="textList"),
+    path("logout", chatlist, name="textList"),
 ]
