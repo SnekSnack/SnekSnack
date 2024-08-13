df941d7384c835fb8c441cfbc0dbb386935059ad
jklooi <jerrilyn.looi@student.unimelb.edu.au>
2024-08-13 11:41:54 +1000

    added some css and layout for frontend

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Box } from '@mui/material'
+
+import "@/app/globals.css"
+
+
+export default function Header() {
+  return (
+    <Box className="header">
+        i am a header
+    </Box>
+  )
+}
