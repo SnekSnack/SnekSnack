f58b8193dca8fd8773888577bb5ebb08b05e523b
daryl-dfkh <daryl.foo@student.unimelb.edu.au>
2024-08-19 17:40:48 +1000

    created Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 import { Box } from '@mui/material'
-
 import "@/app/globals.css"
 
 
-export default function Header() {
-  return (
-    <Box className="header">
-        i am a header
-    </Box>
-  )
+
+interface HeaderProps {
+  userName: string;
 }
+
+
+const Header: React.FC<HeaderProps> = ({ userName }) => {
+
+  return (
+    <header className="header">
+      <button className="header-button">HOME</button>
+      <div className="header-right">
+        <span className="header-message">Hi, NAME {userName}</span>
+        <button className="header-button">LOGOUT</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
