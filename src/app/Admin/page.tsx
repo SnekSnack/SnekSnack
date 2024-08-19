4d72260fdcc278b928e10c0d1f5afdfc4b982cbe
Tyana <tctsai@student.unimelb.edu.au>
2024-08-20 06:58:14 +1000

    changed button size

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,24 +1,23 @@
 import { Box, Button } from "@mui/material";
 import Header from "@/components/Header";
-import "@/app/globals.css"
+import "@/app/globals.css";
 
 // 'Create Assignment' button
 export const MuiButton = () => {
-    return (
-        <div>
-            <Button variant='contained'>+ Create Assignment</Button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Button variant="contained" sx={{ width: '1000px' , height: '50px'}}>+ Create Assignment</Button>
+    </div>
+  );
+};
 
 export default function Admin() {
-    return(
-        <>
-            <Header/>
-            <Box className="content-wrapper" sx={{ paddingTop: '80px' }}>
-                <MuiButton></MuiButton>
-            </Box>
-        </>
-        
-    )
+  return (
+    <>
+      <Header/>
+      <Box className="content-wrapper" sx={{ paddingTop: "80px" }}>
+        <MuiButton></MuiButton>
+      </Box>
+    </>
+  );
 }
