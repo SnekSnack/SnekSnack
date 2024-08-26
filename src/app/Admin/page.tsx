bc27dec4fe590fe8f0ba51d8d14970d368f803b3
Tyana <tctsai@student.unimelb.edu.au>
2024-08-27 07:40:25 +1000

    created assignment box inside assignment page

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Box, Button } from "@mui/material";
 import Header from "@/components/Header";
 import "@/app/globals.css";
---
+++ src/components/HeaderBox.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Link from 'next/link';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+// Define the props type
+interface HeaderBoxProps {
+  title: string;
+  buttonText: string;
+  buttonLink: string;
+}
+
+// INSTEAD OF THE BUTTON TAKING UP THE WHOLE COLUMN
+// SHOULD BE INSIDE HEADER AND HEADER IS CONTAINED WITHIN ANOTHER BOX
+const HeaderBox: React.FC<HeaderBoxProps> = ({ title, buttonText, buttonLink }) => {
+  return (
+    <Box
+      sx={{
+        marginTop: 6,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        // alignItems: 'center',
+        height: 700,
+        width: '80%',
+        bgcolor: 'background.paper',
+        borderRadius: 2,
+        boxShadow: 24,
+        p: 4,
+        mx: 'auto', // Horizontal centering
+        // textAlign: 'left',
+      }}
+    >
+      <h1>{title}</h1>
+      <Link
+        className="hover:underline"
+        href={buttonLink}
+      >
+        {buttonText}
+      </Link>
+    </Box>
+  );
+};
+
+export default HeaderBox;
