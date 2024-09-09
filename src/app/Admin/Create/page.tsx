c9be41732dcad03a78f7c7e75f54745892eeba65
Tyana <tctsai@student.unimelb.edu.au>
2024-09-09 17:09:20 +1000

    temporary text and dropdown added to Admin/Create

--- src/app/Admin/Create/page.tsx
+++ src/app/Admin/Create/page.tsx
@@ -5,6 +5,7 @@ import { Box, Button } from "@mui/material";
 import Header from "@/components/Header";
 import "@/app/globals.css";
 import HeaderBox from '@/components/HeaderBox';
+import Dropdown from '@/components/Dropdown';
 
 export default function Assignment() {
     return(
@@ -19,7 +20,7 @@ export default function Assignment() {
                     // alignItems: 'center',
                     height: 500,
                     width: '80%',
-                    bgcolor: 'background.paper',
+                    bgcolor: 'text.disabled',
                     borderRadius: 2,
                     boxShadow: 24,
                     // p: 4,
@@ -35,7 +36,7 @@ export default function Assignment() {
                     alignItems: 'center',
                     height: 65,
                     width: '100%',
-                    bgcolor: 'text.secondary',
+                    bgcolor: 'text.disabled',
                     borderRadius: 2,
                     // boxShadow: 24,
                     p: 4,
@@ -51,16 +52,80 @@ export default function Assignment() {
                     Select release Date
                     </Link>
                 </Box>
-                <div>Heloooo</div>
+                <Box
+                    sx={{
+                        // marginTop: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        // alignItems: 'center',
+                        height: '100%',
+                        width: '100%',
+                        // bgcolor: 'success.main',
+                        borderRadius: 2,
+                        // boxShadow: 24,
+                        p: 4,
+                        mx: 'auto', // Horizontal centering
+                        // textAlign: 'left',
+                        }}
+                >
+                    {/* <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'flex-start',
+                            alignItems: 'center',
+                            width: '100%',
+                            bgcolor: 'primary.main',
+                            mx: 'auto', // Horizontal centering
+                            textAlign: 'left',
+                            }}
+                    >
+                        <Box
+                            sx={{
+                                // marginTop: 4,
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'flex-start',
+                                alignItems: 'center',
+                                // height: 65,
+                                // width: '100%',
+                                bgcolor: 'secondary.main',
+                                // borderRadius: 2,
+                                // boxShadow: 24,
+                                // p: 4,
+                                mx: 'auto', // Horizontal centering
+                                textAlign: 'left',
+                                }}
+                        >
+                            Heloooo
+                        </Box>
+                        <Box>
+                            <Dropdown></Dropdown>
+                        </Box>
+                        
+                    </Box> */}
+                    {/* testing */}
+                    <Box display="flex" alignItems= 'center'>
+                        <Box 
+                        sx = {{
+                            width: "40%", 
+                            // bgcolor: "lightblue"
+                            }}
+                        >
+                            First Box (for text)
+                        </Box>
+                        <Box width="60%" 
+                        // bgcolor="lightgreen"
+                        >
+                            <Dropdown></Dropdown>
+                        </Box>
+                    </Box>
+
+                    
+                </Box>
+                
             </Box>
     
-            {/* <HeaderBox
-                title="Assignment name" 
-                buttonText="Select release Date" 
-                buttonLink="Login" // Temporary redirect
-            >
-                <div>Heloooo</div>
-            </HeaderBox> */}
         </>
         
     )
